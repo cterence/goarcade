f8160814838d8a3b62f804f22e51766f09f88cad
Térence Chateigné <terence.chateigne@posteo.net>
2025-12-10 15:43:34 +0100

    fix: remove blank chars on newline

--- tests.sh
+++ tests.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-go test -v -run Test_CPU 2>&1 | tee /dev/stderr | sed -E 's/ \([0-9.]+s\)//g; /^ok/d' | sed 's/[^[:print:]\t\n]//g' > /tmp/cpu_test_output.txt
+go test -v -run Test_CPU 2>&1 \
+  | tee /dev/stderr \
+  | sed -E 's/ \([0-9.]+s\)//g; /^ok/d' \
+  | sed 's/[^[:print:]\t\n]//g' \
+  | sed -E 's/^[[:space:]]+$//' \
+  > /tmp/cpu_test_output.txt
 
 awk '
 /<!-- TEST_OUTPUT_START -->/ { print; print "```txt"; system("cat /tmp/cpu_test_output.txt"); print "```"; skip=1; next }
